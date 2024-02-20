a815f7b3b532745fadbf6bc578289fe4b342fbce
OrelAchrak <orelachrak@gmail.com>
2024-02-20 13:17:23 +0200

    added functions

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import factorSum
-import onlyPositive
-import interceptPoint
-import printNumbers
-import analyze
-import listProduct
+from factorSum import factorSum
+from onlyPositive import onlyPositive
+from interceptPoint import interceptPoint
+from printNumbers import printNumbers
+from analyze import analyze
+from listProduct import listProduct
 
-print(factorSum.factorSum(2520))
-print(onlyPositive.onlyPositive(-2))
-print(interceptPoint.interceptPoint((2, 4), (5, -2)))
-printNumbers.printNumbers(2, -3, -1)
+print(factorSum(2520))
+print(onlyPositive(-2))
+print(interceptPoint((2, 4), (5, -2)))
+printNumbers(2, -3, -1)
 print()
-print(listProduct.listProduct([6, 7, 8], [2, 1, 3]))
-print(analyze.analyze("96 , 52.02 , 67.3 , 86.2 , 87.1 , 100"))
+print(listProduct([6, 7, 8], [2, 1, 3]))
+print(analyze("96 , 52.02 , 67.3 , 86.2 , 87.1 , 100"))
